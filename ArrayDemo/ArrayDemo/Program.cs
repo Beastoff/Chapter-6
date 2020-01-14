dc8b520e35409af480b9220054f264db5f61dd51
Beastoff <57193121+Beastoff@users.noreply.github.com>
2020-01-14 15:20:24 -0600

    done

---
+++ ArrayDemo/ArrayDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int howMany;
+
+            Console.WriteLine("How many numbers are you going to enter");
+            howMany = Convert.ToInt32(Console.ReadLine());
+
+            int[] eightNums = new int[howMany];
+            int entry;
+
+            for (int i = 0; i < eightNums.Length; ++i)
+            {
+                Console.WriteLine("Please enter a number >>>>");
+                eightNums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("How do you want to see the info\n" +
+                "1) First to Last?\n" +
+                "2) Last to first\n" +
+                "3) Choose a specific Postion");
+            entry = Convert.ToInt32(Console.ReadLine());
+
+
+        }
+    }
+}
---
+++ TestingArrays/TestingArrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] payRate = { 6.00, 7.35, 8.12, 12.45, 22.22 };
+
+            for (int i = 0; i < payRate.Length; i++)
+            {
+                if (payRate[i] > 11)
+                {
+                    Console.WriteLine(payRate[i].ToString("C"));
+                }
+            }
+        }
+    }
+}
